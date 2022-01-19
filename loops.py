d353b14cc8441810d3f2a32e1e725716b87513fa
Poloeli303 <91678247+Poloeli303@users.noreply.github.com>
2022-01-19 08:59:22 -0700

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,26 @@
+for x in range(1,13):
+    print(x)
+    
+print (" ")
+
+for x in range(8,42,2):
+    print(x)
+
+print (" ")
+
+for x in range(20,-25,-5):
+    print(x)
+    
+print (" ")
+
+Number = int(input("Enter a number "))
+print("your mum "*Number)
+
+print(" ")
+
+import random
+num = 0
+
+while num !=7:
+    num = random.randrange(0,30)
+    print (num)
